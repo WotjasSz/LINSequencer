8fb17e6abf3dade0e0fa8614e2d7ae94f3c5cb41
Wojtek Sztuk <wsztuk@hotmail.com>
2024-12-07 22:27:21 +0100

    Modify to use IoC

--- SequencerUI/App.xaml.cs
+++ SequencerUI/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using SequencerUI.ViewModels;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SequencerUI.ViewModels;
 using SequencerUI.Views;
 using System.Configuration;
 using System.Data;
+using System.Reflection;
 using System.Windows;
 
 
@@ -12,16 +14,43 @@ namespace SequencerUI
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow = new MainWindowView()
-            {
-                DataContext = new MainWindowViewModel()
-            };
-            MainWindow.Show();            
-
             base.OnStartup(e);
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindowView = ServiceProvider.GetRequiredService<MainWindowView>();
+            mainWindowView.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Registering View and ViewModels
+            var assembly = Assembly.GetExecutingAssembly();
+
+            RegisterTypes(services, assembly, "SequencerUI.Views");
+            RegisterTypes(services, assembly, "SequencerUI.ViewModels");
+            
+            // Registartation of the Services
+
+        }
+
+        private void RegisterTypes(IServiceCollection services, Assembly assembly, string namespacePrefix) 
+        { 
+            var types = assembly.GetTypes()
+                .Where(t => t.IsClass && t.Namespace == namespacePrefix)
+                .ToList(); 
+            
+            foreach (var type in types) 
+            { 
+                services.AddTransient(type); 
+            } 
         }
     }
-
 }
--- SequencerUI/Views/MainWindowView.xaml.cs
+++ SequencerUI/Views/MainWindowView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SequencerUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,10 @@ namespace SequencerUI.Views
     /// </summary>
     public partial class MainWindowView : Window
     {
-        public MainWindowView()
+        public MainWindowView(MainWindowViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
 
         //TODO Przenieść kod do view model.
