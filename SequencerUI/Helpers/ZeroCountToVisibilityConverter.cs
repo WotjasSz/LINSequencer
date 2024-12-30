389ef93c5bdb4812a84d4a42f2d19dc041fcb545
Wojtek Sztuk <wsztuk@hotmail.com>
2024-12-30 23:18:32 +0100

    Small improvement of the view. Solve issue with list Drag&Drop. Now droping is possible in whole visible view/zone.

---
+++ SequencerUI/Helpers/ZeroCountToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SequencerUI.Helpers
+{
+    public class ZeroCountToVisibilityConverter : IValueConverter 
+    { 
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
+        { 
+            if (value is int count) 
+            { 
+                return count == 0 ? Visibility.Visible : Visibility.Collapsed; 
+            } 
+            return Visibility.Collapsed; 
+        } 
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+        {
+            if (value is Visibility visibility) 
+            { 
+                return visibility == Visibility.Visible ? 0 : (object)null; 
+            }
+            throw new NotImplementedException();
+        } 
+    }
+}
